157ef3fb65c8b584682f540d60c420d90ea4b8ec
LeThuy <doanlethuy112@gmail.com>
2026-08-26 08:35:56 +0700

    Update ngay1408

--- ngay1408/main.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.getElementById('mobile-menu-btn');
-        const mobileMenu = document.getElementById('mobile-menu');
-
-        menuBtn.addEventListener('click', () => {
-            mobileMenu.classList.toggle('hidden');
-            mobileMenu.classList.toggle('flex');
-        });
