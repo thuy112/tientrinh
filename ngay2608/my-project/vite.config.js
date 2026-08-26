b075c92192b4c4b2b959c0cc468b2e25a63b9ab2
LeThuy <doanlethuy112@gmail.com>
2026-08-26 18:06:54 +0700

    Update ngay2608

---
+++ ngay2608/my-project/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+  server: {
+    hmr: {
+      overlay: false,
+    },
+  },
+})
